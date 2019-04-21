a476f2fb4cae1e42b2a63ceb8e495dae8986c09f
Aleksey <sphota9@gmail.com>
2019-04-21 12:27:41 -0400

    use credentials in env

--- src/email.js
+++ src/email.js
@@ -4,9 +4,9 @@ const email = Router();
 
 email.post('/send', (req, res) => {
   console.log(req.body);
-const mailjet = require ('node-mailjet')
-    .connect('f3a535af49982f605366737c4de10e3c', '5988a995f24fca55d962a0c3d97d61c5')
-const request = mailjet
+  const mailjet = require ('node-mailjet')
+    .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
+  const request = mailjet
     .post("send", {'version': 'v3.1'})
     .request({
         "Messages":[
@@ -27,14 +27,13 @@ const request = mailjet
                 }
         ]
     })
-request
+  request
     .then((result) => {
         res.status(200).json({success: result});
     })
     .catch((err) => {
         res.status(500).json({error: err});
     })
-
 });
 
 export default email;
